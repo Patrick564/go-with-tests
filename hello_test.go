58308e80345e8cd5aefa546ac4c8ba75c527ef31
Patrick Vilchez <pvilchez794@gmail.com>
2022-09-18 21:29:39 -0500

    Refactor test function

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Patrick")
-	want := "Hello, Patrick"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Patrick")
+		want := "Hello, Patrick"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// Tell to test suite that this method is a helper
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
