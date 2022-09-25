0e983b0d4c64d22b5ec4936d5611d174f44b6541
Patrick Vilchez <pvilchez794@gmail.com>
2022-09-24 22:17:39 -0500

    Context module

--- dependency-injection/di.go
+++ dependency-injection/di.go
@@ -1,7 +1,23 @@
-package dependency
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
-func Greet(name string) {
-	fmt.Printf("Hello, %s", name)
+// Dependency Injection is pass a general type (for example as io.Writer) as
+// argument, now you can use different methods who implement `io.Writer interface`
+// as parameters for that method or function.
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
 }
