36464f5ee80549e86b38abe9d30c3d940a39d3bb
Patrick Vilchez <pvilchez794@gmail.com>
2022-09-30 00:00:57 -0500

    Fundamentals done

--- arrays-slices/sum.go
+++ arrays-slices/sum.go
@@ -6,19 +6,47 @@ type Transaction struct {
 	Sum  float64
 }
 
-func BalanceFor(transactions []Transaction, name string) float64 {
-	var balance float64
+type Account struct {
+	Name    string
+	Balance float64
+}
 
-	for _, t := range transactions {
-		if t.From == name {
-			balance -= t.Sum
-		}
-		if t.To == name {
-			balance += t.Sum
-		}
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	}
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
 	}
 
-	return balance
+	return a
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	adjustBalance := func(currentBalance float64, t Transaction) float64 {
+		if t.From == name {
+			return currentBalance - t.Sum
+		}
+		if t.To == name {
+			return currentBalance + t.Sum
+		}
+
+		return currentBalance
+	}
+
+	return Reduce(transactions, adjustBalance, 0.0)
 }
 
 // Sum calculates the total from a slice of numbers.
@@ -43,7 +71,7 @@ func SumAllTails(numbers ...[]int) []int {
 	return Reduce(numbers, sumTail, []int{})
 }
 
-func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+func Reduce[A, B any](collection []A, accumulator func(B, A) B, initialValue B) B {
 	var result = initialValue
 
 	for _, x := range collection {
